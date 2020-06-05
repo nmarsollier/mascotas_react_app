a38244e0996d1fca9432001446ea5bbcf7c1b46e
nmarsollier <nmarsollier@gmail.com>
2020-06-05 17:47:27 -0300

    Mascotas modular

--- src/common/components/LoggedInRoute.tsx
+++ src/common/components/LoggedInRoute.tsx
@@ -1,10 +1,12 @@
-import { mascotasState } from "mascotas_react_store";
+import { useTokenState } from "mascotas_react_store";
 import React from "react";
 import { Route, RouteProps } from "react-router-dom";
 import Welcome from "../../welcome/Welcome";
 
 export default function StateLoggedInRoute(props: RouteProps) {
-  if (mascotasState.token === undefined) {
+  const token = useTokenState()
+
+  if (token === undefined) {
     return (<Route exact path={props.path} component={Welcome} />);
   } else {
     return (<Route exact path={props.path} component={props.component} />);
